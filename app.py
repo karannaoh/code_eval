300314431be9606d0772be662869344d0abdaf20
karannaoh <karanpratapsingh43@gmail.com>
2020-03-31 03:37:50 +0530

    new

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask, request
 app = Flask(__name__)
 import docker
+import os
+
 client = docker.from_env()
 
 
 @app.route('/check', methods = ['POST'])
 def check():
     data = request.get_json()
-    f = open("codes/demo.c", "w")
+    username = "demo" # comes from request
+    f = open("codes/"+username+".c", "w")
     f.write(data['code'])
     f.close()
     temp=b''
     while(temp.decode('utf-8')==''):
         temp =  client.containers.run("comp:latest", "../run.sh", volumes={'/home/naoh/codeeval/codes/':{'bind':'/usr/src/','mode':'rw'}},detach=False, stdout=True)
+    os.remove("codes/"+username+'.c')    
     return temp
 
 if __name__ == '__main__':
--- codes/demo.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h> 
- int main() { 
- printf("321"); 
- return 0; 
- }
